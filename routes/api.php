fad8a2710d15ec3a34f35452a450ced5dfd79cb3
Mahmoud Abdelmoneim <mahmoud.abdelmoneim.dev@gmail.com>
2019-08-09 19:53:04 +0200

    add login api endpoint

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::post('register', 'API\RegisterController@register');
+Route::post('register', 'API\AuthController@register');
+Route::post('login', 'API\AuthController@login');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
